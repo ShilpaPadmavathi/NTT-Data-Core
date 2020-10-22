07f64a77503dc27ed18ec278b5b388596e2286fa
ShilpaPadmavathi <54702632+ShilpaPadmavathi@users.noreply.github.com>
2020-10-22 16:26:01 +0530

    Add files via upload

---
+++ MinVariables.java
@@ -0,0 +1,23 @@
+package GradleDem;
+
+public class MinVariables {
+	
+	public static void main(String[] args) {
+		int intValue=Integer.MIN_VALUE;
+		float floatValue=Float.MIN_VALUE;
+		byte byteValue=Byte.MIN_VALUE;
+		long longValue=Long.MIN_VALUE;
+		double doubleValue=Double.MIN_VALUE;
+		boolean booleanValue=Boolean.TRUE;
+		
+		System.out.println("Smallest Integer Value "+intValue);
+		System.out.println("Smallest Float Value "+floatValue);
+		System.out.println("Smallest Byte Value "+byteValue);
+		System.out.println("Smallest Long Value "+longValue);
+		System.out.println("Smallest Double Value "+doubleValue);
+		System.out.println("Boolean Value "+booleanValue);
+	}
+	
+	
+
+}
